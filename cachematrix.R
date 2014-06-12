c17d9f5477c2510e8570c31066842d3ec1d5f901
jscrane <alqpal123>
2014-06-12 13:30:50 +0100

    pa2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## constructs a special matrix which caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    list(set = 
+             function(y) {
+                 x <<- y
+                 i <<- NULL
+             }, 
+         get = function() x,
+         setinv = function(inv) i <<- inv,
+         getinv = function() i)
 }
 
-
-## Write a short comment describing this function
+## returns the inverse of a caching matrix (see above)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    i <- solve(x$get(), ...)
+    x$setinv(i)
+    i
 }
